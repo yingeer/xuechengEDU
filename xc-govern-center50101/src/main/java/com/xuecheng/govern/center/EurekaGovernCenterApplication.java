be295d07d17c4bf8b44295b83d3ce64cf38a7017
yingeer <2973780290@qq.com>
2020-04-18 18:12:44 +0800

    Eureka服务50101成功启动

---
+++ xc-govern-center50101/src/main/java/com/xuecheng/govern/center/EurekaGovernCenterApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng.govern.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationExtensionsKt;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务注册中心
+ *
+ * @author Ying on 2020/4/18
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaGovernCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGovernCenterApplication.class);
+    }
+}
